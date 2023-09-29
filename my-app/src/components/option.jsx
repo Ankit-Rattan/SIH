32d39f42070af7753d0bfd177cf063156d8f969f
Ankit Rattan <rattanankit2004@gmail.com>
2023-09-30 01:31:52 +0530

    opt

--- my-app/src/components/option.jsx
+++ my-app/src/components/option.jsx
@@ -3,7 +3,22 @@ import React from "react";
 function Option(){
     return(
         <div className="Option">
-
+            <div className="clientOne opt">
+                <h3>Client <br/> One</h3>
+                <p>Client One can register throught here and proced with thier own queries for the furthur discusion.</p>
+                <button>Proceed</button>
+            </div>
+            <div className="clientTwo opt">
+                <h3>Client<br/> Two</h3>
+                <p>Client One can register throught here and proced with thier own queries for the furthur discusion.</p>
+                <button>Proceed</button>
+            </div>
+            <div className="advocate opt">
+                <h3>Advocate</h3>
+                <p>Client One can register throught here and proced with thier own queries for the furthur discusion.</p>
+                <button>Proceed</button>
+                
+            </div>
         </div>
     );
 }
