3700eef4b87cc2c34781e3c0dc77b56dbc21f61b
Ankit <rattanankit2004@gmail.com>
2023-10-22 00:02:35 +0530

    chanign hero and nav

--- my-app/src/components/mainOne.jsx
+++ my-app/src/components/mainOne.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import bg from "./images/heroImg2.jpg";
 
+
+    
+    
 function MainOne(){
+    const myStyle = {
+      
+      backgroundImage:`url(${bg})`,
+      backgroundPosition: `right`,
+      backgroundRepeat : `no-repeat`,
+      backgroundSize : `cover`,
+    };
     return(
-        <div className="MainOne">
+        <div style={myStyle} >
+
+        <div  className="MainOne">
             <h1>Welcome to e-Court</h1>
             <p>Where Justice Meet Proof</p>
 
+        </div>
         </div>
     );
 }
--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import lionLogo from "./images/lion-sym.png";
+import digiLogo from "./images/DigitalInd.png";
 
 function Navbar(){
     return(
@@ -6,7 +8,14 @@ function Navbar(){
 
         <nav>
             <h1>E-Court</h1>
-            <div className="lionImg"></div>
+            <div className="rightIcon">
+            <div className="digitalIndImg">
+                <img src={digiLogo} alt="lionIcon"/>
+            </div>
+            <div className="lionImg">
+                <img src={lionLogo} alt="lionIcon"/>
+            </div>
+            </div>
         </nav>
         <marquee>
             <p>This line is for the latest update regular in justice firm.  This line is for the latest update regular in justice firm</p>
