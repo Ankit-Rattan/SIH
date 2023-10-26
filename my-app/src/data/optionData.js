fbb576aa843a55703dc941fe3522b227e2654f32
Pradneya <pradneya.prog@gmail.com>
2023-10-27 01:29:04 +0530

    Changed UI and made navbar

---
+++ my-app/src/data/optionData.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    classname: "opt1 opt",
+    name: "Client One",
+    text: "Client One can register throught here and proced with thier own queries for the furthur discusion.",
+  },
+  {
+    classname: "opt2 opt",
+    name: "Client Two",
+    text: "Client One can register throught here and proced with thier own queries for the furthur discusion.",
+  },
+  {
+    classname: "opt3 opt",
+    name: "Client Three",
+    text: "Client One can register throught here and proced with thier own queries for the furthur discusion.",
+  },
+];
+
+export default data;
